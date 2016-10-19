f813a28cb8be7e31591e01b4ebd1f7e60d93391d
alu0100836059 <alu0100836059@ull.edu.es>
2016-10-19 16:26:03 +0100

    desplegando a github

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,6 @@ gulp.task('build', function() {
 // gulp.task('deployWiki', function(){
 //   return gulp.src('').pipe(shell(["./scripts/deploy-wiki"]));
 // });
-//
 
 gulp.task('empujar', ['build'],
  shell.task(
