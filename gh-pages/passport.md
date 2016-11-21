37af5f5112382c5b616237232cb0b6f2d56214f7
alu0100836059 <alu0100836059@ull.edu.es>
2016-11-21 01:21:33 +0000

    desplegando a github_apuntes

--- gh-pages/passport.md
+++
@@ -1,3 +0,0 @@
-## Passport
-
-Se trata de un middleware de autenticación para Node.js. Flexible y modular, posee un conjunto de estrategias de autenticacion utilizando el nombre de usuario y contraseña de portales como Facebook, Github, y Twitter entre otros.
--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('wiki-build', function() {
 gulp.task('empujar-libro',
  shell.task("git add .; git commit -am 'desplegando a github_apuntes';"+
     "git remote add repo_apuntes_no_tocar git@github.com:alu0100836059/Apuntes_SYTW.git;"+
-    "git push repo_apuntes_no_tocar master;"+
+    "git push repo_apuntes_no_tocar master;",
 
     // cambiar la coma de arriba por un +  ,borrar esta línea y descomentar las de abajo
     // ";"+
